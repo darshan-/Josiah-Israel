aa525f120d527c8357f554bc3f60e5e267d771e7
Darshan-Josiah Barber <darshan@darshancomputing.com>
2013-05-04 00:36:30 -0700

    Finally figured out how to get z-index working so sliding-div goes *under* whatever is above the div (vertically).

--- ji.dart
+++ ji.dart
@@ -8,12 +8,16 @@ final String TRANSITION_STYLE =
   'margin 0.7s ease-in-out, '
   'opacity 0.7s ease-out';
 
+var listDiv = new DivElement();
+
 void main() {
   fillPq();
 
   for (var p in pq) {
-    document.body.children.add(p);
+    listDiv.children.add(p);
     p.style.margin = '0px 0px 10px 0px';
+    p.style.zIndex = '1';
+    p.style.position = 'relative';
 
     var span = p.query('span.name');
     span.style
@@ -22,9 +26,24 @@ void main() {
       ..cursor = 'pointer';
     span.onClick.listen((_) => setTop(p));
   }
+
+  var h2 = new HeadingElement.h1();
+  h2.innerHtml = 'The Header';
+  h2.style.color = 'green';
+  h2.style.backgroundColor = 'white';
+  h2.style.zIndex = '5';
+  h2.style.opacity = '.99';
+  h2.style.position = 'relative';
+  document.body.children.add(h2);
+
+  listDiv.style.zIndex = '1';
+  listDiv.style.opacity = '.99';
+  document.body.children.add(listDiv);
 }
 
 void setTop(newTop) {
+  if (pq.first == newTop) return;
+
   var div = new DivElement();
   div.style.margin = '0px 0px 10px 0px';
   div.style.padding = '0';
@@ -34,11 +53,11 @@ void setTop(newTop) {
     p.remove();
     div.children.add(p);
     var p2 = copyParagraph(p);
-    document.body.children.add(p2);
+    listDiv.children.add(p2);
     pq.addLast(p2);
   }
 
-  document.body.children.insert(0, div);
+  listDiv.children.insert(0, div);
   div.style.height = '${div.clientHeight}px';
 
   new Timer(new Duration(milliseconds: 2), () {
@@ -46,7 +65,7 @@ void setTop(newTop) {
       div.style.opacity = '0';
       div.style.marginTop = '-${div.clientHeight + 10}px';
       for (var p in pq) {
-        p.style.opacity = '1';
+        p.style.opacity = '.99';
       }
     });
 
@@ -59,7 +78,9 @@ copyParagraph(p1) {
   var p2 = new ParagraphElement();
   p2.innerHtml = p1.innerHtml;
   p2.style.margin = p1.style.margin;
+  p2.style.zIndex = p1.style.zIndex;
   p2.style.opacity = '0';
+  p2.style.position = 'relative';
   p2.style.transition = 'opacity 0.7s ease-in';
   p2.query('span.name').onClick.listen((_) => setTop(p2));
   return p2;
