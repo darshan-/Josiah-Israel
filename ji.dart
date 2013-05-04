2c0ed06cb905dd392468aef031d43e963794c213
Darshan-Josiah Barber <darshan@darshancomputing.com>
2013-05-03 18:15:32 -0700

    Messy new way of transitioning multiple paragraphs at once by putting them in a div and transitioning that.

--- ji.dart
+++ ji.dart
@@ -9,6 +9,58 @@ final String TRANSITION_STYLE =
   'opacity 0.7s ease-out';
 
 void main() {
+  fillPq();
+
+  for (var p in pq) {
+    document.body.children.add(p);
+    p.style.margin = '0px 0px 10px 0px';
+
+    var span = p.query('span.name');
+    span.style
+      ..fontWeight = 'bold'
+      //..color = 'blue'
+      ..cursor = 'pointer';
+    span.onClick.listen((_) => setTop(p));
+  }
+}
+
+void setTop(newTop) {
+  var div = new DivElement();
+  div.style.margin = '0';
+  div.style.padding = '0';
+
+  while (pq.first != newTop) {
+    var p = pq.removeFirst();
+    p.remove();
+    div.children.add(p);
+    var p2 = copyParagraph(p);
+    document.body.children.add(p2);
+    pq.addLast(p2);
+  }
+
+  document.body.children.insert(0, div);
+  div.style.height = '${div.clientHeight}px';
+
+  new Timer(new Duration(milliseconds: 2), () {
+      div.style.transition = TRANSITION_STYLE;
+      div.style.opacity = '0';
+      div.style.height = '0px';
+    });
+
+  new Timer(new Duration(milliseconds: 703), () {
+      div.remove();
+    });
+}
+
+copyParagraph(p1) {
+  var p2 = new ParagraphElement();
+  p2.innerHtml = p1.innerHtml;
+  p2.style.margin = p1.style.margin;
+  p2.query('span.name').onClick.listen((_) => setTop(p2));
+  return p2;
+}
+
+void fillPq() {
   for (var i = 0; i < 3; i++){
     var p = new ParagraphElement();
     p.innerHtml = '<span class="name">Number 1</span> The First Paragraph<br />Still the first paragraph.';
@@ -44,60 +96,4 @@ void main() {
       "The fifth Paragraph<br />" ;
     pq.addLast(p);
   }
-  for (var p in pq) {
-    document.body.children.add(p);
-    p.style.height = '${p.clientHeight}px';
-    p.style.margin = '0px 0px 10px 0px';
-
-    var span = p.query('span.name');
-    span.style
-      ..fontWeight = 'bold'
-      //..color = 'blue'
-      ..cursor = 'pointer';
-    span.onClick.listen((_) => setTop(p));
-  }
-
-  // Initial application of styles shouldn't be transitioned,
-  //  so set transition style after main() returns.
-  new Timer(new Duration(milliseconds: 2), () {
-      for (var p in pq) {
-        p.style.transition = TRANSITION_STYLE;
-      }      
-    });
-}
-
-void setTop(newTop) {
-  while (pq.first != newTop) removeFirst();
-}
-
-void removeFirst() {
-  var p1 = pq.removeFirst();
-  var p2 = new ParagraphElement();
-
-  p2.innerHtml = p1.innerHtml;
-  p2.style.height  = p1.style.height;//'0px';
-  p2.style.opacity = '0';
-  p2.style.margin  = p1.style.margin;//'0';
-  p2.style.transition = TRANSITION_STYLE;
-  p2.query('span.name').onClick.listen((_) => setTop(p2));
-
-  pq.addLast(p2);
-  document.body.children.add(p2);
-
-  var oldHeight  = p1.style.height;
-  var oldOpacity = p1.style.opacity;
-  var oldMargin  = p1.style.margin;
-  p1.style.height  = '0px';
-  p1.style.opacity = '0';
-  p1.style.margin  = '0';
-
-  new Timer(new Duration(milliseconds: 2), () {
-      //p2.style.height = oldHeight;
-      p2.style.opacity = oldOpacity;
-      //p2.style.margin = oldMargin;
-    });
-  
-  new Timer(new Duration(milliseconds: 1000), () {
-      p1.remove();
-    });
 }
